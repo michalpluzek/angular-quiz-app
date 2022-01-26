65ba11e0eef3388e09874e7183acea487ff58104
Michał Płużek <michalpluzek@gmail.com>
2022-01-26 14:27:42 +0100

    feat: Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { QuestionComponent } from './components/question/question.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, QuestionComponent],
+  declarations: [AppComponent, WelcomeComponent, QuestionComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
