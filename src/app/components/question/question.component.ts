0c121703eb159edb44d72a7fd2245b6d2626e67c
Michał Płużek <michalpluzek@gmail.com>
2022-01-26 16:10:49 +0100

    feat: Add tamplate and style to question component

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faStopwatch,
+  faChevronLeft,
+  faChevronRight,
+  faSyncAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  faStopwatch = faStopwatch;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+  faSyncAlt = faSyncAlt;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
