af855ba78052be7ddc28117b600e74d0db5df297
Michał Płużek <michalpluzek@gmail.com>
2022-01-28 10:32:40 +0100

    feat: Add counter and answer method

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -5,6 +5,7 @@ import {
   faChevronRight,
   faSyncAlt,
 } from '@fortawesome/free-solid-svg-icons';
+import { interval } from 'rxjs';
 import { QuestionService } from 'src/app/service/question.service';
 
 @Component({
@@ -23,12 +24,17 @@ export class QuestionComponent implements OnInit {
   public currentQuestion: number = 0;
   public points: number = 0;
   counter: number = 60;
+  correctAnswer: number = 0;
+  wrongAnswer: number = 0;
+  interval$: any;
+  progress: string = '0';
 
   constructor(private questionsService: QuestionService) {}
 
   ngOnInit(): void {
     this.name = localStorage.getItem('name')!;
     this.getAllQuestions();
+    this.startCounter();
   }
 
   getAllQuestions() {
@@ -43,5 +49,48 @@ export class QuestionComponent implements OnInit {
 
   previousQuestion() {
     this.currentQuestion > 0 && this.currentQuestion--;
+  }
+
+  answer(currentQue: number, option: any) {
+    if (option.correct) {
+      this.points += 10;
+      this.correctAnswer++;
+    } else {
+      this.points -= 10;
+      this.wrongAnswer++;
+    }
+    currentQue < this.questionList.length && this.currentQuestion++;
+  }
+
+  startCounter() {
+    this.interval$ = interval(1000).subscribe((val) => {
+      this.counter--;
+      if (this.counter === 0) {
+        this.currentQuestion++;
+        this.counter = 60;
+        this.points -= 10;
+      }
+    });
+    setTimeout(() => {
+      this.interval$.unsubscribe();
+    }, 600000);
+  }
+
+  stopCounter() {
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  }
+
+  resetCounter() {
+    this.stopCounter();
+    this.counter = 60;
+    this.startCounter();
+  }
+
+  resetQuiz() {
+    this.resetCounter();
+    this.getAllQuestions();
+    this.points = 0;
+    this.currentQuestion = 0;
   }
 }
